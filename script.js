dcc95d9f6e7c3b2463cbde662264f76e0346ca1a
kasireddihimabindu <kasireddi.himabindu@gmail.com>
2022-09-11 20:31:07 +0530

    Replace MC with back space and add functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let btns = document.querySelectorAll(".num-button");
 let allBtns = document.querySelectorAll(".button");
 let resultBox = document.querySelector("#result-box");
 let clearBtn = document.querySelector('#clear');
+let backspacebtn = document.querySelector('#backspace')
+let symbols = document.querySelectorAll(".calc-action-btn")
 
 let total = document.querySelector("#total");
 
@@ -11,13 +13,35 @@ let allBtnSpread = [...allBtns];
 btnSpread.forEach((button, i) => {
     button.addEventListener("click", () => {
       // Inner Values for calculator
-  
-      if (resultBox.innerHTML == "0") {
+      let resultInnerHTML = resultBox.innerHTML
+      if (resultInnerHTML == "0") {
         resultBox.innerHTML = "";
+      };
+      let lastChar = resultInnerHTML.charAt(resultInnerHTML.length-1); 
+      
+      let symbolArr = ['%','+','-','/','.','*'];
+      let dontAllowChar = '';
+      // if(!symbolArr.includes(lastChar)){
+      //   value= btns[i].innerHTML;
+      //   resultBox.innerHTML += value;
+      // }
+      for(let i=0; i<symbolArr.length; i++){
+            if(lastChar == symbolArr[i]){
+              dontAllowChar = symbolArr[i];
+            }
       }
-  
+
       let value = btns[i].innerHTML;
-      resultBox.innerHTML += value;
+      if(value != dontAllowChar){
+        if(symbolArr.includes(value) && symbolArr.includes(lastChar)){
+          resultBox.innerHTML = resultBox.innerHTML.slice(0, -1);
+          console.log(resultBox.innerHTML);
+        }
+        resultBox.innerHTML += value;
+      }
+
+// 9*
+
     });
   });
 // Function to evalute Strings
@@ -36,4 +60,14 @@ console.log(evaluate(allInputs));
 // Clear all Inputs
 clearBtn.addEventListener('click', ()=> {
     resultBox.innerHTML = "0";
+})
+//backspace Click
+backspacebtn.addEventListener('click',()=> {
+  let resultInnerHTML = resultBox.innerHTML
+  if(resultInnerHTML.length>1){
+    resultBox.innerHTML = resultBox.innerHTML.slice(0, -1);
+  } else{
+    resultBox.innerHTML="0";
+  }
+  
 })
